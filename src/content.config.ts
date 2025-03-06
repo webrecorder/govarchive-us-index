9b924e81728d628f6c6c513edd6a2fd637a69157
emma <hi@emma.cafe>
2025-03-06 16:10:02 -0500

    add basic content setup

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+import { file } from 'astro/loaders';
+
+const archiveCollections = defineCollection({
+  loader: file("collections/collections.yaml"),
+  schema: z.object({
+    endpoint: z.string().endsWith("/").default("https://app.browsertrix.com/"),
+    org: z.string(),
+    collection: z.string(),
+    customUrl: z.string().url().optional()
+  })
+});
+
+export const collections = {collections: archiveCollections}
