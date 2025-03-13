e21d6744a902b4b6a7a27655b61b62a65280be01
Ilya Kreymer <ikreymer@gmail.com>
2025-03-12 19:43:53 -0700

    fix banner height

--- public/scripts/proxyui.js
+++ public/scripts/proxyui.js
@@ -181,7 +181,6 @@ class Ae extends Pe{}Ae.directiveName="unsafeSVG",Ae.resultType=2;const Te=ke(Ae
         margin: 0;
         padding: 0;
         width: 100%;
-        height: 100%;
         z-index: 1000000000;
       }
       .banner {
